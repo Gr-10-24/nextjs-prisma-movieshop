3191ac2525dd3b9c505d50ace07d5a8e666bb5c3
Chathura Rathnadheera <kotuduragekumara@gmail.com>
2025-02-20 13:52:10 +0100

    Customer all movies (#18)
    * updated carousel all movie
    
    updated for broken urls
    
    * updated movie carousel
    
    update the issue with rendering missing url alt

--- src/components/ui/customer/carousel-allmovies.tsx
+++ src/components/ui/customer/carousel-allmovies.tsx
@@ -95,8 +95,8 @@ export default function CarouselAllMovies() {
               >
                 <div className="p-1">
                   <Card className="transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-xl">
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-3xl font-semibold">
+                    <CardContent className="flex aspect-square items-center p-6">
+                      <span className="text-sm justify-end">
                         {<DialogMovie movie={movie} />}
                       </span>
                     </CardContent>
@@ -125,16 +125,16 @@ export default function CarouselAllMovies() {
         opts={{
           align: "start",
         }}
-        className="w-full max-w-2xl"
+        className="w-full h-max-240"
       >
         <h1 className="mt-6">K-T movies</h1>
         <CarouselContent>
           {K_TMovies.map((movie) => (
-            <CarouselItem key={movie.id} className="md:basis-1/2 lg:basis-1/10">
+            <CarouselItem key={movie.id} className="md:basis-1/2 lg:basis-1/5">
               <div className="p-1">
                 <Card className="transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-xl">
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">
+                  <CardContent className="flex aspect-square items-center  p-6">
+                    <span className="text-sm justify-end">
                       {<DialogMovie movie={movie} />}
                     </span>
                   </CardContent>
@@ -159,16 +159,16 @@ export default function CarouselAllMovies() {
         opts={{
           align: "start",
         }}
-        className="w-full max-w-2xl"
+        className="w-full h-max-240"
       >
         <h1 className="mt-6">U-Z movies</h1>
         <CarouselContent>
           {U_ZMovies.map((movie) => (
-            <CarouselItem key={movie.id} className="md:basis-1/2 lg:basis-1/10">
+            <CarouselItem key={movie.id} className="md:basis-1/2 lg:basis-1/5">
               <div className="p-1">
                 <Card className="transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-xl">
                   <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">
+                    <span className="text-sm justify-end">
                       {<DialogMovie movie={movie} />}
                     </span>
                   </CardContent>
