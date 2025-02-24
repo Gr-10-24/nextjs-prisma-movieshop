97910f99e957d35a5d8ba055a6ab00d4b6d5d9af
Timmoten <tim.nyman@telia.com>
2025-02-24 10:15:59 +0100

    removed unused imports

--- src/components/movies/viewmovie.tsx
+++ src/components/movies/viewmovie.tsx
@@ -1,69 +1,61 @@
-"use client"
-import { GetMovie, MovieData } from "@/app/actions/viewmovie";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+"use client";
+import { MovieData } from "@/app/actions/viewmovie";
+import { TableCell, TableRow } from "@/components/ui/table";
 import { toast } from "@/hooks/use-toast";
 import { Role } from "@prisma/client";
 import { EditDialog } from "./movieEdit";
-import { Actor } from "next/font/google";
 import { DeleteTodo } from "@/app/actions/editupdateMovie";
- 
- 
-export default function ViewMovie({ data }: { data:MovieData}){
- 
-  const directors = data.starring
-  ?.filter(star => star.role === Role.DIRECTOR)
-  .map(star => star.person.name)
-  .join(', ') || 'No Directors';
- 
-const actors = data.starring
-  ?.filter(star => star.role === Role.ACTOR)
-  .map(star => star.person.name)
-  .join(', ') || 'No Actors';
- 
- 
- 
-    return(
-                    <TableRow>
-                        <TableCell className="font-medium">{data.title}</TableCell>
-                        <TableCell>
-                          {data.description.length > 50
-                             ? `${data.description.slice(0, 50)}...`
-                                 : data.description}
-                        </TableCell>
-                        <TableCell title={data.imageUrl ?? "No Image"}>
-                          {data.imageUrl
- 
-                           ? `${data.imageUrl.slice(0, 30)}...`
-                            : "No Image"}
-                        </TableCell>
-                        <TableCell>{data.genres.map(genre => genre.name).join(', ')}</TableCell>
-                        <TableCell>{actors}</TableCell>
-                        <TableCell>{directors}</TableCell>
-                        <TableCell>{data.price}</TableCell>
-                        <TableCell>{data.stock}</TableCell>
-                        <TableCell>{data.releaseDate}</TableCell>
-                        <TableCell>{data.runtime}</TableCell>
-                        <TableCell><EditDialog data={data} /></TableCell>
-                        <TableCell><button  onClick={() =>DeleteTodo(data.id).then(() =>
-                                        toast({
-                                           title: "Delete successful",
-                                       description: data.title + "Was deleted",
-                                             variant: "destructive",
-                                                  })
-                                        )
-                                         } className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 border border-red-700 rounded">Delete</button>
-                        </TableCell>
-                    </TableRow>
- 
- 
+
+export default function ViewMovie({ data }: { data: MovieData }) {
+  const directors =
+    data.starring
+      ?.filter((star) => star.role === Role.DIRECTOR)
+      .map((star) => star.person.name)
+      .join(", ") || "No Directors";
+
+  const actors =
+    data.starring
+      ?.filter((star) => star.role === Role.ACTOR)
+      .map((star) => star.person.name)
+      .join(", ") || "No Actors";
+
+  return (
+    <TableRow>
+      <TableCell className="font-medium">{data.title}</TableCell>
+      <TableCell>
+        {data.description.length > 50
+          ? `${data.description.slice(0, 50)}...`
+          : data.description}
+      </TableCell>
+      <TableCell title={data.imageUrl ?? "No Image"}>
+        {data.imageUrl ? `${data.imageUrl.slice(0, 30)}...` : "No Image"}
+      </TableCell>
+      <TableCell>{data.genres.map((genre) => genre.name).join(", ")}</TableCell>
+      <TableCell>{actors}</TableCell>
+      <TableCell>{directors}</TableCell>
+      <TableCell>{data.price}</TableCell>
+      <TableCell>{data.stock}</TableCell>
+      <TableCell>{data.releaseDate}</TableCell>
+      <TableCell>{data.runtime}</TableCell>
+      <TableCell>
+        <EditDialog data={data} />
+      </TableCell>
+      <TableCell>
+        <button
+          onClick={() =>
+            DeleteTodo(data.id).then(() =>
+              toast({
+                title: "Delete successful",
+                description: data.title + "Was deleted",
+                variant: "destructive",
+              })
+            )
+          }
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-3 border border-red-700 rounded"
+        >
+          Delete
+        </button>
+      </TableCell>
+    </TableRow>
   );
 }
