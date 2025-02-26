b56f5fa06f6bc215ab637b5e7bfe8e4eb30e1fda
Timmoten <tim.nyman@telia.com>
2025-02-26 10:54:11 +0100

    changing colors and buttons

--- src/components/signup-dialog.tsx
+++ src/components/signup-dialog.tsx
@@ -1,29 +1,30 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
 import SignUpForm from "./signup-form";
 
 export function SignupDialog() {
-
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant={"secondary"} className="border-2" >{"Register" }</Button>
+        <Button variant={"default"} className="border-2">
+          {"Register"}
+        </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-            <DialogTitle></DialogTitle>
+          <DialogTitle></DialogTitle>
         </DialogHeader>
         <SignUpForm />
       </DialogContent>
     </Dialog>
-  )
+  );
 }
