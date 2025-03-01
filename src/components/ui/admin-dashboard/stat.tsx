6fe4d5f74cf55871178bbd7e455ca13e5ac1e701
chathura Kumara Rathnadheera <kotuduragekumara@gmail.com>
2025-03-01 16:12:12 +0100

    Admin dashboar
    added ban page
    deleted landing page

--- src/components/ui/admin-dashboard/stat.tsx
+++ src/components/ui/admin-dashboard/stat.tsx
@@ -4,20 +4,23 @@ import SalesGraph from "@/components/ui/admin-dashboard/sales-graph";
 import ScrollAreaStock from "@/components/ui/admin-dashboard/scroll-Area_stock";
 import FetchMovies from "../../../app/actions/customer-movie";
 
+
 export default async function AdminDashboard() {
   const order = await FetchOrders();
   const movies = await FetchMovies();
 
   return (
-    <div className="flex ">
-      <div className="w-200 h-100 mt-8 ml-8">
+    <div className="flex flex-col items-center justify-center mt-8">
+      <h1 className="text-2xl font-semibold">Business Analytics and Admin Panel</h1>
+      <div>
+        <OrderStat order={order} />
+      </div>
+      <div className="flex w-200 h-100 mt-8 ml-8">
         <SalesGraph order={order} />
         <div className="ml-20">
           <ScrollAreaStock movie={movies} />
         </div>
-      </div>
-      <div>
-        <OrderStat order={order} />
+        
       </div>
     </div>
   );
