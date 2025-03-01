cfa167d2a1aafbdd75f15c4e0ba7aaac54df03f8
chathura Kumara Rathnadheera <kotuduragekumara@gmail.com>
2025-03-01 16:17:38 +0100

    Admin dashboard
    added ban page
    deleted landing page

--- src/app/customer-movies/page.tsx
+++ src/app/customer-movies/page.tsx
@@ -56,7 +56,7 @@ export default function TastMovies() {
   return (
     <div>
       <div className="px-12 pt-12 mb-12 ">
-        <h1 className="text-3xl font-bold justify-center mb-8">MOVIES</h1>
+        <h1 className="text-3xl font-bold justify-center mb-8">MOVIE LIBRARY</h1>
         <div className="flex items-center bp-4 ">
           {/* render a search bar on the browser. */}
           <Input
